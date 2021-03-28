aadd44c1ed32d9c8284921be965cdcea200202f9
jeri <diltsjeri@gmail.com>
2021-03-28 13:58:44 -0400

    updated charges controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :charges, only: [:new, :create]
+  devise_for :users
+  resources :posts
+  root to: 'posts#index'
 end
